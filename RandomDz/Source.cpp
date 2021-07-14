1a980a5b6a1e23c2f56d891fdac02a1c601e9e99
yandutka@mail.ru <yandutka@mail.ru>
2021-07-14 19:10:03 +0300

    commit

--- RandomDz/Source.cpp
+++ RandomDz/Source.cpp
@@ -14,14 +14,31 @@ void main()
 	int arr[n]{};
 
 	srand(time(NULL));
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n;)
 	{
-		arr[i] = rand() % 10;
-	}
-	cout << endl;
+		bool counter = false;
+		int newRandom = rand() % 10;	
 
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[j] == newRandom)
+			{
+				counter = true;
+				break;
+			}
+		}
+		
+		if (counter != true)
+		{
+			arr[i] = newRandom;
+			i++;
+		}
+		
+	}
+	
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << tab;
 	}
+
 }
