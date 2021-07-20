ad2efd2da1370aaee91ddbc65655f186f9ee9eb9
yandutka@mail.ru <yandutka@mail.ru>
2021-07-20 22:29:48 +0300

    commit

---
+++ Arrays2D/Arrays2D.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int ROWS = 8; //Кол-во строк
+	const int COLS = 5; //Кол-во элементов строки (столбиков)
+	int arr[ROWS][COLS] 
+	{
+		{},
+		{1,2,3},
+		{4,5,6},
+		{7,8,9}
+	};
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			//Счётчик i нумерует строки
+			//Счётчик j нумерует столбики
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+ }
